5f4b324c924119fd49c172d85c7997899749a9b2
Pedro Cordeiro <pedro@naroga.com.br>
2015-11-05 13:02:15 -0200

    Improved unit tests for PriorityQueue

--- src/Querker/QueueBundle/Tests/PriorityQueueTest.php
+++ src/Querker/QueueBundle/Tests/PriorityQueueTest.php
@@ -15,10 +15,13 @@ class PriorityQueueTest extends \PHPUnit_Framework_TestCase
         $priorityQueue = new PriorityQueue();
         $priorityQueue->insert('test1', 1);
         $priorityQueue->insert('test2', 2);
-        $this->assertEquals($priorityQueue->count(), 2);
+        $priorityQueue->insert('test3', 1);
+        $this->assertEquals($priorityQueue->count(), 3);
         $this->assertEquals($priorityQueue->extract(), 'test2');
-        $this->assertEquals($priorityQueue->count(), 1);
+        $this->assertEquals($priorityQueue->count(), 2);
         $this->assertEquals($priorityQueue->extract(), 'test1');
+        $this->assertEquals($priorityQueue->count(), 1);
+        $this->assertEquals($priorityQueue->extract(), 'test3');
         $this->assertEquals($priorityQueue->count(), 0);
     }
 }
